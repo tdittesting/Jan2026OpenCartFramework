7a41aaeb908005eb31ab14bd7bcf589d34182e4f
TDIT <tdit.testing@gmail.com>
2026-08-10 09:30:34 +0530

    Changes made as advised

--- src/main/java/com/qa/Trading/pages/AddCart.java
+++ src/main/java/com/qa/Trading/pages/AddCart.java
@@ -1,9 +1,11 @@
 package com.qa.Trading.pages;
 
+import org.openqa.selenium.By;
+
 public class AddCart {
- int i = 10;
+ private By cart = By.id("cart");
  
  public void addCart() {
-	 System.out.println("Adding to the cart method");
+	 System.out.println("Adding to the cart method"+cart);
  }
 }
